fe34dc2547199dc37f6cae1c74a8dc2dc77e410e
Neha Nikhat <nehanikhat18@gmail.com>
2021-06-13 20:50:56 +0530

    Tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-7xl">Hi, I'm Neha</h1>
+    </div>
+  );
 }
 
 export default App;
