e1b6202bb27bdb798ad4ad83d1c18a2ce29c433f
Neha Nikhat <nehanikhat18@gmail.com>
2021-07-09 21:58:24 +0530

    Added 2 more projects

--- src/images/index.js
+++ src/images/index.js
@@ -1,28 +1,44 @@
 const IMAGES = {
-  menu: require("./menu.png").default,
-  close: require("./close.png").default,
-  light: require("./light.png").default,
-  dark: require("./dark.png").default,
+  menu: require("./icons/menu.png").default,
+  close: require("./icons/close.png").default,
+  light: require("./icons/light.png").default,
+  dark: require("./icons/dark.png").default,
 
-  html: require("./html.png").default,
-  css: require("./css.png").default,
-  tailwind: require("./tailwind.jpg").default,
-  react: require("./react.png").default,
-  js: require("./js.png").default,
-  nodejs: require("./nodejs.png").default,
-  firebase: require("./firebase.png").default,
-  git: require("./git.png").default,
+  html: require("./skills/html.png").default,
+  css: require("./skills/css.png").default,
+  tailwind: require("./skills/tailwind.jpg").default,
+  react: require("./skills/react.png").default,
+  js: require("./skills/js.png").default,
+  nodejs: require("./skills/nodejs.png").default,
+  firebase: require("./skills/firebase.png").default,
+  git: require("./skills/git.png").default,
 
-  open: require("./open.png").default,
-  repo: require("./repo.png").default,
+  open: require("./icons/open.png").default,
+  repo: require("./icons/repo.png").default,
+
+  portfolio: {
+    portfolio_1: require("./projects/portfolio/portfolio_1.JPG").default,
+    portfolio_2: require("./projects/portfolio/portfolio_2.JPG").default,
+    portfolio_3: require("./projects/portfolio/portfolio_3.JPG").default,
+    portfolio_4: require("./projects/portfolio/portfolio_4.JPG").default,
+    portfolio_5: require("./projects/portfolio/portfolio_5.JPG").default,
+  },
 
   the_venue: {
-    the_venue_1: require("./projects/the_venue/the_venue_1.png").default,
+    the_venue_1: require("./projects/the_venue/the_venue_1.jpg").default,
     the_venue_2: require("./projects/the_venue/the_venue_2.png").default,
     the_venue_3: require("./projects/the_venue/the_venue_3.png").default,
     the_venue_4: require("./projects/the_venue/the_venue_4.png").default,
-    the_venue_5: require("./projects/the_venue/the_venue_5.png").default,
-    the_venue_6: require("./projects/the_venue/the_venue_6.png").default,
+    the_venue_5: require("./projects/the_venue/the_venue_5.jpg").default,
+    the_venue_6: require("./projects/the_venue/the_venue_6.jpg").default,
+  },
+
+  instagram: {
+    instagram_1: require("./projects/instagram/instagram_1.jpg").default,
+    instagram_2: require("./projects/instagram/instagram_2.jpg").default,
+    instagram_3: require("./projects/instagram/instagram_3.jpg").default,
+    instagram_4: require("./projects/instagram/instagram_4.jpg").default,
+    instagram_5: require("./projects/instagram/instagram_5.jpg").default,
   },
 
   ipl: {
@@ -34,9 +50,9 @@ const IMAGES = {
     ipl_6: require("./projects/ipl/ipl_6.png").default,
   },
 
-  linkedin: require("./linkedin.png").default,
-  mail: require("./mail.png").default,
-  download: require("./download.png").default,
+  linkedin: require("./icons/linkedin.png").default,
+  mail: require("./icons/mail.png").default,
+  download: require("./icons/download.png").default,
 };
 
 export default IMAGES;
